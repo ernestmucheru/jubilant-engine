6892991746f0428f7f0b7a4a6b1fe8ee0b7c43d1
Your Name <you@example.com>
2021-07-17 17:14:17 +0300

    initial

--- awards/settings.py
+++ awards/settings.py
@@ -25,10 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
     'authy',
 ]
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
