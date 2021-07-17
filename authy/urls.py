dd82fa602071a2e15a798f0d53e1e9989a016169
Your Name <you@example.com>
2021-07-17 15:17:11 +0300

    created my login template and added in the login payh to the urls

--- authy/urls.py
+++ authy/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = "main"
+app_name = "authy"
 
 urlpatterns = [
     # path("",views.homepage, name="homepage"),
 
     path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
 ]
