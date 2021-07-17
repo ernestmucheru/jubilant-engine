aa9834d40ea643639ec5db4eb5aba4d8aee42b74
Your Name <you@example.com>
2021-07-17 14:55:15 +0300

    added registration function and configured the urls to working state

--- authy/urls.py
+++ authy/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path("",views.homepage, name="homepage"),
+    # path("",views.homepage, name="homepage"),
 
     path("register", views.register_request, name="register"),
 ]
