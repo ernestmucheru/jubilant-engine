b1ef6edd1eb4463256a2ccb2b98afea84c41cf80
Your Name <you@example.com>
2021-07-18 06:37:40 +0300

    correcetd my paths

--- authy/urls.py
+++ authy/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.registerPage, name='register'),
-    path('login/', views.loginPage, name='login')
+    path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutPage, name='logout')
 
 ]
