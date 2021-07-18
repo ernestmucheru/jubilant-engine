860780a3c547b180e2067378259856092e272228
Your Name <you@example.com>
2021-07-18 06:25:17 +0300

    updated my login function

--- authy/views.py
+++ authy/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 def registerPage(request):
     form = CreateUserForm()
@@ -10,12 +11,21 @@ def registerPage(request):
         if form.is_valid():
             form.save()
             user = form.cleaned_data.get('username')
-            messages.success(request,'Account for {username} has been created successfully')
+            messages.success(request,'Account for ' + user +' has been created successfully')
             return redirect('login')
 
 
     context = {'form':form}
     return render(request,'accounts/register.html', context)
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
     context = {}
     return render(request,'accounts/login.html', context)
