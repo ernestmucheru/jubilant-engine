77837d37d489c37edf8e49114f61f6bd80a14300
Your Name <you@example.com>
2021-07-18 06:15:49 +0300

    setup flash messages

--- authy/views.py
+++ authy/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
+from django.contrib import messages
 
 def registerPage(request):
     form = CreateUserForm()
@@ -8,6 +9,8 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,'Account for {username} has been created successfully')
             return redirect('login')
 
 
