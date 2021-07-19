0f7ac175f791686ced56a776228abe2d29a71040
Your Name <you@example.com>
2021-07-19 13:01:14 +0300

    created a projects view

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Projects
+from .models import Projects,Category
 # Register your models here.
 
 
 admin.site.register(Projects)
+admin.site.register(Category)
--- homepage/models.py
+++ homepage/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100,null=False ,blank=False)
+
+    def __str__(self):
+        return self.name
+
 class Projects(models.Model):
     alt = models.CharField(max_length=60)
+    title = models.CharField(max_length=100, null=True)
     image = models.ImageField(upload_to="")
-    decription = models.CharField(max_length=300)
+    description = models.CharField(max_length=300)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.alt
