07f1b8cccd959c43defdc0c4dd5e8d630ec4fb0e
Your Name <you@example.com>
2021-07-20 08:37:39 +0300

    created uploads section

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import Projects
+from .models import Category, Projects
 
 
 # Create your views here.
@@ -8,7 +8,8 @@ from .models import Projects
 def index(request):
     context = {}
     projects = Projects.objects.all()
-    context["projects"] = projects
+    # categroies = Category.objects.all()
+    context["projects"] = projects = {'projects': projects}
 
 
     return render(request, 'index.html',context)
@@ -18,8 +19,17 @@ def about(request):
     return render(request, 'about.html')
 
 def upload(request):
+    projects = Projects.objects.all()
+    context = {'projects':projects}
 
-    return render(request, 'upload.html')
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+
+        print('data:', data)
+        print('image:', image)
+
+    return render(request, 'upload.html',context)
 
 def viewProject(request, id):
     project = Projects.objects.get(id=id)
